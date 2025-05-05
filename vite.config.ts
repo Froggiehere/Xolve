993ebfd72e118d120aaaf6b2ba1a526b37c50787
Ali Said Güdekli <161058074+Froggiehere@users.noreply.github.com>
2025-05-05 12:03:40 +0000

    Update to deploy to Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Xolve/', // Replace 'Xolve' with your actual project name
   plugins: [react()],
-  base: '/Xolve'
-})
+});
